52fc67dac81a632dd3decad36c32d82156a359c5
Kaique-py <66974354+Kaique-py@users.noreply.github.com>
2023-05-20 20:19:53 -0300

    página de contatos

--- core/views.py
+++ core/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+#Aqui no arquivo views vamos criar todas as páginas que forem existir no meu site.
+
+#Iniciamos criando uma página chamada index, que será nossa front page.
 def index(request):
     return render(request, 'core/index.html')
+
+#Na sequência vamos criando as páginas a seguir:
+def contact(request):
+    return render(request, 'core/contact.html')
