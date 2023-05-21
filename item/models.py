f665c8e7525669178767f233c5516f959378e9b6
Kaique-py <66974354+Kaique-py@users.noreply.github.com>
2023-05-20 22:51:08 -0300

    Criação do models

--- item/models.py
+++ item/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Category(models.Model):
+class Categoria(models.Model):
     nome = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categorias' #Aqui eu altero a forma como o Django vai transformar em plural as palavras que eu escrever.
