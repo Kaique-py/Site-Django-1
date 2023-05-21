497fb4d029049eae1a208a2e00274d22dbe28290
Kaique-py <66974354+Kaique-py@users.noreply.github.com>
2023-05-20 23:07:49 -0300

    Criação do campo Item (produto)

--- item/admin.py
+++ item/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Categoria
+from .models import Categoria, Item
 
 admin.site.register(Categoria)
+admin.site.register(Item)
--- item/models.py
+++ item/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -6,4 +7,20 @@ class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
     class Meta:
+        ordering = ('nome',) #Aqui ordenamos as categorias em ordem alfabética.
         verbose_name_plural = 'Categorias' #Aqui eu altero a forma como o Django vai transformar em plural as palavras que eu escrever.
+
+    def __str__(self):
+        return self.nome #Aqui é para aparecerem os nomes das categorias, ao invés de 'categoria object'
+    
+    #Após criarmos as categorias, vamos criar os itens que irão se enquadrar em alguma categoria.
+
+class Item(models.Model):
+    categoria = models.ForeignKey(Categoria, related_name='items', on_delete=models.CASCADE) #Esse Cascade, como visto na matéria Banco de Dados, quer dizer que se o conjunto Categoria for deletado, todos os itens atrelados a ele também serão deletados, por efeito cascata.
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField(blank=True, null=True)
+    preco = models.FloatField()
+    imagem = models.ImageField(upload_to='item_images', blank=True, null=True)
+    vendido = models.BooleanField(default=False)
+    vendedor = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    data_criacao = models.DateTimeField(auto_now_add=True)
